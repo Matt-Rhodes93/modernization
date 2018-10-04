7d00b57d81cb95e35eae129d6180297d5619b3fa
Matt <matt.rhodes.ee7a@statefarm.com>
2018-10-04 12:51:40 -0600

    circuit breaker

--- applications/album-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
+++ applications/album-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.superbiz.moviefun.blobstore.BlobStore;
